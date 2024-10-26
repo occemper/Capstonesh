c02be60de0bff451efa1b5b51c81965434b853aa
occemper <ravilre22@gmail.com>
2024-10-27 00:56:10 +0300

    routing 006

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import Navigation from './routes/navigation/navigation.component'
 import Home from './routes/home/home.component'
 
 function App() {
@@ -6,7 +7,10 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path='/' element={< Home />} />
+        <Route path='/' element={< Navigation />}>
+          <Route index element={< Home />} />
+          <Route path='/shop' element='shop' />
+        </Route>
       </Routes>
     </div>
   )
