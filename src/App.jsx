e08dcdaf40fc33a55eebf1999abf61837ec50ab4
occemper <ravilre22@gmail.com>
2024-10-28 00:33:21 +0300

    Auth 001

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Navigation from './routes/navigation/navigation.component'
 import Home from './routes/home/home.component'
+import SignIn from './routes/sign-in/sign-in.component'
 
 
 
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={< Navigation />}>
           <Route index element={< Home />} />
           <Route path='/shop' element='shop' />
+          <Route path='/sign-in' element={<SignIn />} />
         </Route>
       </Routes>
     </div>
